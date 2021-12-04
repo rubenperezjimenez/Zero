3a11db3f6c354a8f0a4b657b96fa7b92c3fd7b63
Ruben Perez Jimenez <ruben.perez.mty@gmail.com>
2021-12-04 10:02:11 -0600

    Zero Zero

---
+++ CreateGitRepository_step1.bat
@@ -0,0 +1,8 @@
+#git user.name "Ruben Perez Jimenez"
+#git user.email "ruben.perez.mty@gmail.com"
+git init
+git add .
+git commit -m "Commit Zero"
+git remote add origin https://github.com/rubenperezjimenez/test.git
+git branch -M main
+git push -u origin main
